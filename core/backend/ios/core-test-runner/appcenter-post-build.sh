828b9a011e43d0dd7d572d3fa5f36766b8382780
Daniel Toby <41296254+DanielToby@users.noreply.github.com>
2022-02-25 12:45:44 -0500

    Test print output in shell.

--- core/backend/ios/core-test-runner/appcenter-post-build.sh
+++ core/backend/ios/core-test-runner/appcenter-post-build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+counter(){
+    for file in "$1"/*
+    do
+    if [ -d "$file" ]
+    then
+            echo "$file"
+            counter "$file"
+    fi
+    done
+}
+
+counter "$APPCENTER_OUTPUT_DIRECTORY"
+
 appcenter test run xcuitest \
   --async \
   --app $APP \
