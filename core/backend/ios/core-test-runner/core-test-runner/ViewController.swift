c9e37bfd0770333b901c9e546e55553a5022447e
Daniel Toby <41296254+DanielToby@users.noreply.github.com>
2022-03-01 13:20:17 -0500

    Add test result check.

--- core/backend/ios/core-test-runner/core-test-runner/ViewController.swift
+++ core/backend/ios/core-test-runner/core-test-runner/ViewController.swift
@@ -18,12 +18,18 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, UIDo
     private func runTests () {
         let host = IModelJsHost.sharedInstance()
         let bundlePath = Bundle.main.bundlePath
-        let mainPath = bundlePath.appending ("/Assets/main.js")
+        let mainPath = bundlePath.appending("/Assets/main.js")
         let main = URL(fileURLWithPath: mainPath)
         let client = MobileAuthorizationClient(viewController: self)
         print("(ios): Running tests.")
         host.loadBackend(main, withAuthClient: client, withInspect: true) { (numFailed: UInt32) in
             print("(ios): Finished running tests.")
+            let testOutputPath = bundlePath.appending("/Assets/junit_results.xml")
+            let fileManager = FileManager.default
+            if !fileManager.fileExists(atPath: testOutputPath) {
+                print("(ios): Test results not found. Path: \(testOutputPath)")
+                exit(-1)
+            }
             exit(Int32(numFailed))
         }
     }
