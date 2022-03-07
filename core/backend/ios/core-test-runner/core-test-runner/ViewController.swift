dd3799f2b9f21b2fd75caf28b02f3d8c557bc959
Daniel Toby <41296254+DanielToby@users.noreply.github.com>
2022-03-07 14:57:52 -0500

    Updated termination method.

--- core/backend/ios/core-test-runner/core-test-runner/ViewController.swift
+++ core/backend/ios/core-test-runner/core-test-runner/ViewController.swift
@@ -4,11 +4,15 @@
 *--------------------------------------------------------------------------------------------*/
 
 import UIKit
+import os
 
 import IModelJsNative
 
 class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, UIDocumentPickerDelegate {
     private var webView : WKWebView? = nil
+    private let logger = Logger(subsystem: "com.bentley.core-test-runner", category: "tests")
+    private var numFailed = -1
+    private var testsFinished = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,21 +20,30 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, UIDo
     }
 
     private func runTests () {
+        
         let host = IModelJsHost.sharedInstance()
         let bundlePath = Bundle.main.bundlePath
         let mainPath = bundlePath.appending("/Assets/main.js")
         let main = URL(fileURLWithPath: mainPath)
         let client = MobileAuthorizationClient(viewController: self)
         print("(ios): Running tests.")
-        host.loadBackend(main, withAuthClient: client, withInspect: true) { (numFailed: UInt32) in
-            print("(ios): Finished running tests.")
-            let testOutputPath = bundlePath.appending("/Assets/junit_results.xml")
-            let fileManager = FileManager.default
-            if !fileManager.fileExists(atPath: testOutputPath) {
-                print("(ios): Test results not found. Path: \(testOutputPath)")
-                exit(-1)
-            }
-            exit(Int32(numFailed))
+        logger.log("(ios)(logger): Running tests.")
+        host.loadBackend(main, withAuthClient: client, withInspect: true) { [self] (numFailed: UInt32) in
+            self.numFailed = Int(numFailed)
+            self.testsFinished = true
+        }
+        
+        while !testsFinished {
+        }
+        
+        logger.log("(ios)(logger): Finished running tests (2).")
+        NSLog("(ios)(nslog): Finished running tests (2).")
+        
+        let testOutputPath = bundlePath.appending("/Assets/junit_results.xml")
+        let fileManager = FileManager.default
+        if !fileManager.fileExists(atPath: testOutputPath) {
+            print("(ios): Test results not found. Path: \(testOutputPath)")
+            exit(-1)
         }
     }
 }
