9e82b1a782320bf490a2fbe6ab8d0c8a08ffe41b
Daniel Toby <41296254+DanielToby@users.noreply.github.com>
2022-03-08 17:35:01 -0500

    Restore previous xcode project.

--- core/backend/ios/core-test-runner/core-test-runner-tests/core_test_runner_tests.swift
+++ core/backend/ios/core-test-runner/core-test-runner-tests/core_test_runner_tests.swift
@@ -2,7 +2,7 @@
 //  core_test_runner_tests.swift
 //  core-test-runner-tests
 //
-//  Created by Daniel Toby on 3/8/22.
+//  Created by Daniel Toby on 2/15/22.
 //  Copyright © 2022 Bentley Systems, Inc. All rights reserved.
 //
 
@@ -14,8 +14,5 @@ class core_test_runner_tests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-        
-        let finishedTestsLabel = app.staticTexts["Finished Running Tests."]
-        XCTAssert(finishedTestsLabel.waitForExistence(timeout: 30))
     }
 }
